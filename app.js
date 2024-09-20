8df8753f76d9f9fc03e338c31406fe1c99a2fe2b
Todd Scharien <todd.scharien@gov.bc.ca>
2024-09-20 10:30:55 -0700

    Add dummy caseload API endpoint

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello world! This is the visitz-api.' });
 });
 
+app.get('/caseload', (req, res) => {
+  res.json({message: 'This is the caseload API endpoint.'});
+});
+
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
 });
