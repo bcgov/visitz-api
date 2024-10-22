51f0515f389b98f50d11da5a93a58fc60ec0a011
hannah-macdonald1 <hamacdonald@deloitte.ca>
2024-10-22 11:11:45 -0600

    Add colon to console log

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-  console.log(`API is running on port ${port}`);
+  console.log(`API is running on port: ${port}`);
 }
 bootstrap();
