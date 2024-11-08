3f640b5f07fcda9216773bdb9f99161c8b302fa2
hannah-macdonald1 <hamacdonald@deloitte.ca>
2024-11-08 10:42:12 -0700

    Add env variable for endpoint

--- src/common/constants/upstream-constants.ts
+++ src/common/constants/upstream-constants.ts
@@ -1,11 +1,13 @@
 const baseUrlEnvVarName = 'UPSTREAM_BASE_URL';
 const supportNetworkEndpointEnvVarName = 'SUPPORT_NETWORK_ENDPOINT';
 const inPersonVisitsEndpointEnvVarName = 'IN_PERSON_VISITS_ENDPOINT';
+const attachmentsEndpointEnvVarName = 'ATTACHMENTS_ENDPOINT';
 const idirUsernameHeaderField = 'x-idir-username';
 
 export {
   baseUrlEnvVarName,
   supportNetworkEndpointEnvVarName,
   inPersonVisitsEndpointEnvVarName,
+  attachmentsEndpointEnvVarName,
   idirUsernameHeaderField,
 };
