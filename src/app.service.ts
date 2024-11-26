ab7ea0649eb11a11f7eb11caa2d08d8a618aec1b
hannah-macdonald1 <hamacdonald@deloitte.ca>
2024-11-26 14:34:53 -0700

    remove root endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello world! This is the visitz-api.';
-  }
-
   getCaseload(): string {
     return 'This is the caseload API endpoint.';
   }
