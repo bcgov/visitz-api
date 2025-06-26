6f7412c7144818d4211afff494c791f4db0a8c3c
Hannah MacDonald <hamacdonald@deloitte.ca>
2025-06-26 13:42:42 -0600

    Add organizational filtering to API access

---
+++ src/entities/employee.entity.ts
@@ -0,0 +1,6 @@
+export const EmployeeExample = {
+  Id: 'Id Here',
+  'Login Name': 'IDIR Here',
+  'Employment Status': 'Status Here',
+  'Primary Organization Id': 'Primary Organization Id Here',
+};
