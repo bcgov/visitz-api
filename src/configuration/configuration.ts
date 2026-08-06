af8c4876368920116e52673a1009734a0c6b6398
Hannah MacDonald <hannah.e.macdonald@gov.bc.ca>
2026-08-06 15:12:38 -0600

    remove memo env variable

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -194,7 +194,6 @@ export default () => ({
     caseActivityPlan: process.env.CASE_ACTIVITY_PLAN_ENDPOINT ?? ' ',
     incidentActivityPlan: process.env.INCIDENT_ACTIVITY_PLAN_ENDPOINT ?? ' ',
     srActivityPlan: process.env.SR_ACTIVITY_PLAN_ENDPOINT ?? ' ',
-    memoActivityPlan: process.env.MEMO_ACTIVITY_PLAN_ENDPOINT ?? ' ',
   },
   fileUpload: {
     maxFileSizeBytes: parseInt(process.env.MAX_FILE_SIZE_BYTES) ?? 5242880,
